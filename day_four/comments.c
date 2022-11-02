24e18555dbf16893df45c4d337e51bf2a9b8e87f
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2022-11-02 14:11:40 +0000

    worked on the four dir

---
+++ day_four/comments.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    /*
+        * This is a program
+        * That has only comments
+        * Do you see yourself
+        * Coding in a comment
+        * Or you cant do same
+        * Don't get discourage yet
+        * Out of the night that covers me
+        * Black as the pit from pole to pole 
+        * I thank whatever God may be
+        * For my unconquerable soul
+        * In the fell clutch of circumstance
+        * I have not winced or cried aloud
+        * Under the bludgeonings of chance
+        * My head is bloody but unbowed
+        * Beyond this place of wrath and tears
+        * Looms but the horrors of the shade
+        * And yet the menace of the years
+        * Finds and shall find me unafraid
+        * It matters not how strait the gate 
+        * How charged with punishment the scrolls
+        * I am the master of my life 
+        * I am the captain of my soul
+        
+    */
+}
---
+++ day_four/format.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    /*
+        * A simple program
+        * That prints
+        * the format of the inputed value
+        * usiing
+        * %d
+        * %o
+        * %c
+        * %s
+        * %f
+        * %i
+        
+    */ 
+    printf("%d\n", 66);
+    printf("%c\n", 65);
+    printf("%o\n", 66);
+    printf("%x\n", "ff");
+    
+
+
+
+}

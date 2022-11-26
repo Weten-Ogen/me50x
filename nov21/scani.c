c76b9500a5f67413e9d6f95d84da212312bdab77
Weten-Ogen <alphaoware@gmail.com>
2022-11-26 11:30:52 +0000

    practice on gets and scanf

--- nov21/scani.c
+++ nov21/scani.c
@@ -3,12 +3,25 @@
 
 int main(void)
 {
-    int a, b , sum;
-    printf("Enter a number : ");
-    scanf("%lf", &a);
-    printf("Enter a Second number : ");
-    scanf("%lf", &b);
-    sum = a + b;
-    printf("%lf + %lf = %lf\n",a,b,sum);
+    // int a, b , sum;
+    // printf("Enter a number : ");
+    // scanf("%lf", &a);
+    // printf("Enter a Second number : ");
+    // scanf("%lf", &b);
+    // sum = a + b;
+    // printf("%lf + %lf = %lf\n",a,b,sum);
+    // char name[50];
+    // printf("Hello world to ....");
+    // gets(name);
+    // printf("...%s\n", name);
+    int a, b, c;
+
+    printf("Enter a number nums\n");
+    gets(a,b,c);
+    int avg = (a + b + c)//2;
+    pr
+
+
+    
 
 }
